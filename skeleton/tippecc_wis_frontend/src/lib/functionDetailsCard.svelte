fba72eb1c8d526c01f10532a2c642f59e36795df
fabian <schreifab@gmail.com>
2023-12-12 14:10:59 +0100

    Add skeleton popup function to root layout. Popup description can now be shown for a field name

--- skeleton/tippecc_wis_frontend/src/lib/functionDetailsCard.svelte
+++ skeleton/tippecc_wis_frontend/src/lib/functionDetailsCard.svelte
@@ -1,8 +1,31 @@
 <script lang="ts">
-    import type {FunctionDetails} from "$lib/interfaces";
+    import type {FunctionDetails, Dataset} from "$lib/interfaces";
     export let functionDetails : FunctionDetails; 
+
+    import { popup } from '@skeletonlabs/skeleton';
+    import type { PopupSettings } from '@skeletonlabs/skeleton';
+
+    
+
+
+    const popupHover: PopupSettings = {
+	event: 'hover',
+	target: 'popupHover',
+	placement: 'bottom'
+    };
+
 </script>
 
-<p>
-    {functionDetails.id}
-</p>
+<div class = 'card'>
+    {#each functionDetails.datasets as dataset}
+    <div>
+        <p class = "[&>*]:pointer-events-none" use:popup={popupHover}>
+            {dataset.name}
+        </p>
+        <div class="card p-4 variant-filled-secondary" data-popup="popupHover">
+            <p>{dataset.desc}</p>
+            <div class="arrow variant-filled-secondary" />
+        </div>
+    </div>
+    {/each}
+</div>
